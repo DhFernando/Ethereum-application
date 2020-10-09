64241be91f8ac255a7538b4e0fe628990b20b772
dhfernando <hdilshan.fernando11@gmail.com>
2020-10-10 01:23:30 +0530

    fetch acounts

---
+++ test/inbox.test.js
@@ -0,0 +1,29 @@
+const assert =  require('assert')
+const ganache =  require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+
+const { interface , bytecode } = require('../compile')
+
+
+
+let accounts;
+beforeEach(async () =>{
+    //get list of all accounts
+    accounts = await web3.eth.getAccounts();
+            
+    //use one of those accounts to deploy
+
+})
+
+
+describe('Inbox' , ()=>{
+    it('deploys a contract' ,()=>{
+        console.log(accounts);
+    })
+})
+
+
+
+
+
