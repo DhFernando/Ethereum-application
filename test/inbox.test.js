eb564ba3762084c4b8ff0a3a0652df37fd50a5ea
dhfernando <hdilshan.fernando11@gmail.com>
2020-10-10 14:20:38 +0530

    get data from contract

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -5,21 +5,31 @@ const web3 = new Web3(ganache.provider())
 
 const { interface , bytecode } = require('../compile')
 
-
-
 let accounts;
+let inbox ;
+let _message
+let INIT_MSG  = "Hi ! there ,"
 beforeEach(async () =>{
     //get list of all accounts
     accounts = await web3.eth.getAccounts();
             
     //use one of those accounts to deploy
 
+    //the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data : bytecode , arguments : [INIT_MSG] })
+        .send({from : accounts[0] , gas:'1000000'})
 })
 
 
 describe('Inbox' , ()=>{
     it('deploys a contract' ,()=>{
-        console.log(accounts);
+        assert.ok(inbox.options.address)
+    })
+
+    it('Get message', async ()=>{
+        _message =  await inbox.methods.message().call();
+        assert.equal(_message , INIT_MSG)
     })
 })
 
